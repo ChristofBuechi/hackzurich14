a1e323b7a626586354aaca17a9dab57e60c7211d
longstone <git@longstone.ch>
2014-10-11 16:35:18 +0200

    initial controller for video latest and fixed a bug in videolatest rest, where the ordering is now reversed

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -9,25 +9,28 @@
  * Main module of the application.
  */
 angular
-  .module('publicApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('publicApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            }).when('/latest', {
+                templateUrl: 'views/latest.html',
+                controller: 'LatestVideos'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
