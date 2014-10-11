78c34595c08c1c35b017267462917b0812046f0b
longstone <stone@hutzl>
2014-10-11 19:56:10 +0200

    some dirty workarounds for filetype

--- app/services/aws.js
+++ app/services/aws.js
@@ -5,6 +5,7 @@
 var BUCKET_NAME = 'foovie';
 
 var fs = require('fs');
+var mime = require('mime');
 
 var aws = require('aws-sdk');
 aws.config.loadFromPath('./config_aws.json');
@@ -12,10 +13,10 @@ aws.config.loadFromPath('./config_aws.json');
 var s3 = new aws.S3();
 
 
-var uploadVideo = function (remoteFilename, fileName) {
-    var fileBuffer = fs.readFileSync(fileName);
-    var metaData = getContentTypeByFile(fileName);
-    console.log('AWS uploader received order: remoteFilename: '+remoteFilename+" fileName: "+fileName);
+var uploadVideo = function (remoteFilename, filePath) {
+    var fileBuffer = fs.readFileSync(filePath);
+    var metaData = getContentTypeByFile(filePath);
+    console.log('AWS uploader received order: remoteFilename: ' + remoteFilename + " fileName: " + filePath);
     s3.putObject({
         ACL: 'public-read',
         Bucket: BUCKET_NAME,
@@ -23,27 +24,43 @@ var uploadVideo = function (remoteFilename, fileName) {
         Body: fileBuffer,
         ContentType: metaData
     }, function (error, response) {
-        console.log('uploaded file[' + fileName + '] to [' + remoteFilename + '] as [' + metaData + ']');
-        fs.unlink(fileName, function (err) {
+        console.log('uploaded file[' + filePath + '] to [' + remoteFilename + '] as [' + metaData + ']');
+        fs.unlink(filePath, function (err) {
             if (err) throw err;
-            console.log('successfully deleted: '+ fileName);
+            console.log('successfully deleted: ' + filePath);
         });
     });
 
     return "https://s3-us-west-2.amazonaws.com/" + BUCKET_NAME + "/" + remoteFilename;
 };
 
-function getContentTypeByFile(fileName) {
+function getContentTypeByFile(filePath) {
     var rc = 'application/octet-stream';
-    var fileNameLowerCase = fileName.toLowerCase();
+    var fileNameLowerCase = filePath.toLowerCase();
 
-    if (fileNameLowerCase.indexOf('.html') >= 0) rc = 'text/html';
-    else if (fileNameLowerCase.indexOf('.css') >= 0) rc = 'text/css';
-    else if (fileNameLowerCase.indexOf('.json') >= 0) rc = 'application/json';
-    else if (fileNameLowerCase.indexOf('.js') >= 0) rc = 'application/x-javascript';
-    else if (fileNameLowerCase.indexOf('.png') >= 0) rc = 'image/png';
-    else if (fileNameLowerCase.indexOf('.jpg') >= 0) rc = 'image/jpg';
-    else if (fileNameLowerCase.indexOf('.mp4') >= 0) rc = 'video/mp4';
+    if (fileNameLowerCase.indexOf('.html') >= 0) {
+        rc = 'text/html';
+    }
+    else if (fileNameLowerCase.indexOf('.css') >= 0) {
+        rc = 'text/css';
+    }
+    else if (fileNameLowerCase.indexOf('.json') >= 0) {
+        rc = 'application/json';
+    }
+    else if (fileNameLowerCase.indexOf('.js') >= 0) {
+        rc = 'application/x-javascript';
+    }
+    else if (fileNameLowerCase.indexOf('.png') >= 0) {
+        rc = 'image/png';
+    }
+    else if (fileNameLowerCase.indexOf('.jpg') >= 0) {
+        rc = 'image/jpg';
+    }
+    else if (fileNameLowerCase.indexOf('.mp4') >= 0) {
+        rc = 'video/mp4';
+    }else{
+        rc = mime.lookup(filePath);
+    }
 
     return rc;
 }
