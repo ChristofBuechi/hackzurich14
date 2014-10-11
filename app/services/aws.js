cf8a499a1c5d48342a2f773517be045979cac160
Christof Buechi <christof.buechi@gmail.com>
2014-10-11 14:01:54 +0200

    added getContentTypeByFile

--- app/services/aws.js
+++ app/services/aws.js
@@ -30,4 +30,18 @@ var uploadVideo = function (remoteFilename, fileName) {
     return "https://s3-us-west-2.amazonaws.com/" + BUCKET_NAME + "/" + fileName;
 };
 
+function getContentTypeByFile(fileName) {
+    var rc = 'application/octet-stream';
+    var fileNameLowerCase = fileName.toLowerCase();
+
+    if (fileNameLowerCase.indexOf('.html') >= 0) rc = 'text/html';
+    else if (fileNameLowerCase.indexOf('.css') >= 0) rc = 'text/css';
+    else if (fileNameLowerCase.indexOf('.json') >= 0) rc = 'application/json';
+    else if (fileNameLowerCase.indexOf('.js') >= 0) rc = 'application/x-javascript';
+    else if (fileNameLowerCase.indexOf('.png') >= 0) rc = 'image/png';
+    else if (fileNameLowerCase.indexOf('.jpg') >= 0) rc = 'image/jpg';
+
+    return rc;
+}
+
 module.exports = uploadVideo;
--- rest/usercreate.js
+++ rest/usercreate.js
@@ -4,17 +4,17 @@
 var User = require('./../app/models/user');
 var createUser = function (req, res) {
 
-    var user = new User(); 		// create a new instance of the Bear model
-    console.log('create user called with req:');
-    console.log(req);
-    user.username = req.body.username;
-    user.secret = req.body.secret;
-    user.save(function (err) {
-        if (err) {
-            res.send(err);
-        }
-    });
-    res.json({ message: 'user created!' });
-}
-;
+        var user = new User(); 		// create a new instance of the Bear model
+        console.log('create user called with req:');
+        console.log(req);
+        user.username = req.body.username;
+        user.secret = req.body.secret;
+        user.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+        });
+        res.json({ message: 'user created!' });
+    }
+    ;
 module.exports = createUser;
