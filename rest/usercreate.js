35a631b004bdb6176be104cb7cc4ff1c7b4d6139
longstone <git@longstone.ch>
2014-10-11 03:02:35 +0200

    multible changes

---
+++ rest/usercreate.js
@@ -0,0 +1,20 @@
+/**
+ * Created by stone on 11.10.2014.
+ */
+var User = require('./../app/models/user');
+var createUser = function (req, res) {
+
+    var user = new User(); 		// create a new instance of the Bear model
+    console.log('create user called with req:');
+    console.log(req);
+    user.username = req.body.username;
+    user.secret = req.body.secret;
+    user.save(function (err) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'user created!' });
+    });
+
+};
+module.exports=createUser;
