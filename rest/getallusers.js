ae8b8e17246ee6341e86c50aff103560fa79df9c
longstone <git@longstone.ch>
2014-10-11 05:02:43 +0200

    some sleep could might be useful

--- rest/getallusers.js
+++ rest/getallusers.js
@@ -2,6 +2,8 @@
  * Created by stone on 11.10.2014.
  */
 var User = require('./../app/models/user');
+
+
 var getallusers = function (req, res) {
     var query = User.find();
     query.select('username');
