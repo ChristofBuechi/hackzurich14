56678873f77edf1d4afba8dfcbed6b45e9071903
longstone <git@longstone.ch>
2014-10-12 04:15:49 +0200

    en one liner :D

--- rest/getallusers.js
+++ rest/getallusers.js
@@ -5,15 +5,11 @@ var Video = require('./../app/models/video');
 
 
 var getallusers = function (req, res) {
-    var query = Video.find();
-    query.select('username');
-    query.exec(function (err, person) {
-        if (err) {
-            console.error(err);
-            res.send(err);
-        }
-        res.json(person);
-    });
+
+    Video.distinct('username', function (err, usernames) {
+        res.json(usernames);
+    })
+
 };
 module.exports = getallusers;
 
